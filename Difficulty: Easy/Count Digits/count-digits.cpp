b619c8a626dde91deebb9b9e45a5c883a353076d
SUBASHRI E <115530600+subashri-e@users.noreply.github.com>
2024-07-30 23:58:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        std::string numStr = std::to_string(N);
+        int count = 0;
+        for (char c : numStr) {
+            int digit = c - '0';  
+            if (digit != 0 && N % digit == 0) { 
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
