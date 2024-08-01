0e1d68dc07a41349ee7cfc61c1ae913720e6ac64
SUBASHRI E <115530600+subashri-e@users.noreply.github.com>
2024-08-01 23:26:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Third largest element/third-largest-element.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int thirdLargest(vector<int> &arr) {
+        int arr_size = arr.size();
+        if (arr_size < 3) {                            
+            return -1; 
+        } 
+        int first = INT_MIN, second = INT_MIN, third = INT_MIN;
+        for (int i = 0; i < arr_size; i++) {
+            if (arr[i] > first) {
+                third = second;
+                second = first;
+                first = arr[i];
+            } else if (arr[i] > second) {
+                third = second;
+                second = arr[i];
+            } else if (arr[i] > third) {
+                third = arr[i];
+            }
+        }
+        return third;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.thirdLargest(arr) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
