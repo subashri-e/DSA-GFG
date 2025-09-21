8c1c9766ce55702797dd8d92cdfcd1f30277099d
SUBASHRI E <subashrielango@gmail.com>
2025-09-21 11:16:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        if(s1.length()!=s2.length()) return false;
+        int[] freq = new int[26];
+        for(int i=0;i<s1.length();i++){
+            freq[s1.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s2.length();i++){
+            freq[s2.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
